fcc1379b52b9603bd6aaff8fffbdd5f4be898a07
Harshna Goratela <harshna.goratela@gmail.com>
2020-03-01 22:40:08 +0530

    aligned categories dynamically

--- src/components/explorecategories.js
+++ src/components/explorecategories.js
@@ -8,86 +8,35 @@ const ExploreCategories = ({ data }) => {
   return (
     <div className="pt-5">
       <h3 className="text-center p-4">Explore Our Furniture Range (All Categories)</h3>
-      <Container className="">
-        <Row className="d-inline-flex flex-row flex-wrap">
-          <Col className="col-lg-3">
-            {data.map(({ node }, i) => (
-              <Card className="col-lg-3" key={node.id}>
-                <Img fluid={node.icon.fluid} />
-              </Card>
-            ))}
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-          <Col className="col-lg-3">
-            <Card.Text>Field-Item</Card.Text>
-          </Col>
-        </Row>
-      </Container>
       <Container>
-        <Row >
-          <CardDeck className="d-inline-flex flex-row flex-wrap w-100">
-            {data.map(({ node }, i) => (
-              <Card className="col-lg-3" key={node.id}>
+        <Row className="d-flex flex-row flex-wrap">
+          {data.map(({ node }, i) => (
+            <Col className="d-flex col-lg-3 p-5 align-items-center">
+              <Card className="w-100" key={node.id}>
                 <Img fluid={node.icon.fluid} />
               </Card>
-            ))}
-            <Card>
-              <Card.Img className="col-lg-3" variant="top" src="/img-600-400.png" />
-            </Card>
-            <Card>
-              <Card.Img className="col-lg-3" variant="top" src="/img-600-400.png" />
-            </Card>
-            <Card>
-              <Card.Img className="col-lg-3" variant="top" src="/img-600-400.png" />
-            </Card>
-            <Card>
-              <Card.Img className="col-lg-3" variant="top" src="/img-600-400.png" />
-            </Card>
-            <Card>
-              <Card.Img className="col-lg-3" variant="top" src="/img-600-400.png" />
-            </Card>
-          </CardDeck>
+            </Col>
+          ))}
+          <Col className="d-flex col-lg-3 p-5 align-items-center">
+            <Card className="w-100">
+              <Card.Img variant="top" src="/img-600-400.png" />
+            </Card></Col>
+          <Col className="d-flex col-lg-3 p-5 align-items-center">
+            <Card className="w-100">
+              <Card.Img variant="top" src="/img-600-400.png" />
+            </Card></Col>
+          <Col className="d-flex col-lg-3 p-5 align-items-center">
+            <Card className="w-100">
+              <Card.Img variant="top" src="/img-600-400.png" />
+            </Card></Col>
+          <Col className="d-flex col-lg-3 p-5 align-items-center">
+            <Card className="w-100">
+              <Card.Img variant="top" src="/img-600-400.png" />
+            </Card></Col>
+          <Col className="d-flex col-lg-3 p-5 align-items-center">
+            <Card className="w-100">
+              <Card.Img variant="top" src="/img-600-400.png" />
+            </Card></Col>
         </Row>
       </Container>
     </div>
