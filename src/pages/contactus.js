57f668ba6575d93441d929b79ba2b8edc470cfc0
Harshna Goratela <harshna.goratela@gmail.com>
2020-03-03 22:07:29 +0530

    added google maps

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -2,14 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { FaRegUser, FaMailBulk, FaMobileAlt } from "react-icons/fa"
+import { GoLocation, GoDeviceMobile } from "react-icons/go"
 
 // reactstrap components
 import {
   Button,
-  InputGroup,
   Form,
-  FormControl,
   Container,
   Row,
   Col
@@ -34,18 +32,18 @@ const ContactusPage = ({ data, location }) => {
                 <form id="contact-form" action="/emailsuccess" method="post" role="form" data-netlify="true" data-netlify-honeypot="bot-field">
                   <input type="hidden" name="form-name" value="contact-form" />
                   <Form.Group controlId="formBasicName">
-                    <Form.Control name="name" type="name" placeholder="Enter your name" required/>
+                    <Form.Control name="name" type="name" placeholder="Enter your name" required />
                   </Form.Group>
 
                   <Form.Group controlId="formBasicEmail">
-                    <Form.Control name="email" type="email" placeholder="Enter email" required/>
+                    <Form.Control name="email" type="email" placeholder="Enter email" required />
                     <Form.Text className="text-muted">
                       We'll never share your email with anyone else.
                     </Form.Text>
                   </Form.Group>
 
                   <Form.Group controlId="formBasicPhonenumber">
-                    <Form.Control name="phonenumber" type="number" placeholder="Enter phone number" required/>
+                    <Form.Control name="phonenumber" type="number" placeholder="Enter phone number" required />
                     <Form.Text className="text-muted">
                       We'll contact you on this number. We never share your phone number with anyone else.
                     </Form.Text>
@@ -62,9 +60,23 @@ const ContactusPage = ({ data, location }) => {
                 </form>
               </Col>
               <Col className="ml-auto mr-auto" md="5">
+              <div className="info info-horizontal">
+                  <div className="icon icon-info">
+                    <GoDeviceMobile/>
+                  </div>
+                  <div className="description">
+                    <h4 className="info-title">Give us a ring</h4>
+                    <p>
+                      Rajiv Soni <br></br>
+                      +91 79 23211927 <br></br>
+                      Mon - Sat, 9:00 am - 7:00 pm<br></br>
+                      Sunday Closed
+                      </p>
+                  </div>
+                </div>
                 <div className="info info-horizontal mt-5">
                   <div className="icon icon-info">
-                    <i className="now-ui-icons location_pin"></i>
+                    <GoLocation />
                   </div>
                   <div className="description">
                     <h4 className="info-title">Find us at the office</h4>
@@ -73,20 +85,8 @@ const ContactusPage = ({ data, location }) => {
                       Nr. Hyundai Show Room, <br></br>
                       GIDC, Sector-28, <br></br>
                       Gandhinagar - Gujarat
-                      </p>
-                  </div>
-                </div>
-                <div className="info info-horizontal">
-                  <div className="icon icon-info">
-                    <i className="now-ui-icons tech_mobile"></i>
-                  </div>
-                  <div className="description">
-                    <h4 className="info-title">Give us a ring</h4>
-                    <p>
-                      Rajiv Soni <br></br>
-                      +91 79 23211927 <br></br>
-                      Mon - Sat, 9:30-19:30
-                      </p>
+                    </p>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.6045717768607!2d72.6523447!3d23.2574723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c2c78eaaaaaab%3A0x48702434ad275226!2sUmiya%20Interior!5e0!3m2!1sen!2sin!4v1583253217598!5m2!1sen!2sin" width="100%" height="300" frameborder="0" style={{border:0}} allowfullscreen="true"></iframe>
                   </div>
                 </div>
               </Col>
