e1b5b7aea82a77b8ffb638ab1d33d07794b01b92
Harshna Goratela <harshna.goratela@gmail.com>
2020-03-07 17:15:16 +0530

    getting contact details from contentful

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { BLOCKS } from "@contentful/rich-text-types"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { GoLocation, GoDeviceMobile } from "react-icons/go"
@@ -14,6 +15,13 @@ import {
 } from "react-bootstrap";
 
 const ContactusPage = ({ data, location }) => {
+
+  const homepageData = data.contentfulHomepage
+
+  const Text = ({ children }) => <p className="m-0">{children}</p>
+  const options = {
+    renderText: text => text.split('\n').flatMap((text, i) => [i > 0 && <br />, text]),
+  }
 
   return (
     <Layout>
@@ -60,18 +68,13 @@ const ContactusPage = ({ data, location }) => {
                 </form>
               </Col>
               <Col className="ml-auto mr-auto" md="5">
-              <div className="info info-horizontal">
+                <div className="info info-horizontal">
                   <div className="icon icon-info">
-                    <GoDeviceMobile/>
+                    <GoDeviceMobile />
                   </div>
                   <div className="description">
                     <h4 className="info-title">Give us a ring</h4>
-                    <p>
-                      Rajiv Soni <br></br>
-                      +91 79 23211927 <br></br>
-                      Mon - Sat, 9:00 am - 7:00 pm<br></br>
-                      Sunday Closed
-                      </p>
+                    {documentToReactComponents(homepageData.contactUsPageContactDetails.json,options)}
                   </div>
                 </div>
                 <div className="info info-horizontal mt-5">
@@ -81,12 +84,9 @@ const ContactusPage = ({ data, location }) => {
                   <div className="description">
                     <h4 className="info-title">Find us at the office</h4>
                     <p>
-                      Plot No. 905/1, <br></br>
-                      Nr. Hyundai Show Room, <br></br>
-                      GIDC, Sector-28, <br></br>
-                      Gandhinagar - Gujarat
+                      {documentToReactComponents(homepageData.contactUsPageContactAddress.json,options)}
                     </p>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.6045717768607!2d72.6523447!3d23.2574723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c2c78eaaaaaab%3A0x48702434ad275226!2sUmiya%20Interior!5e0!3m2!1sen!2sin!4v1583253217598!5m2!1sen!2sin" width="100%" height="300" frameborder="0" style={{border:0}} allowfullscreen="true"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3665.6045717768607!2d72.6523447!3d23.2574723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395c2c78eaaaaaab%3A0x48702434ad275226!2sUmiya%20Interior!5e0!3m2!1sen!2sin!4v1583253217598!5m2!1sen!2sin" width="100%" height="300" frameborder="0" style={{ border: 0 }} allowfullscreen="true"></iframe>
                   </div>
                 </div>
               </Col>
@@ -107,3 +107,16 @@ const ContactusPage = ({ data, location }) => {
 }
 
 export default ContactusPage
+
+export const pageQuery = graphql`
+  query {
+    contentfulHomepage {
+      contactUsPageContactDetails {
+        json
+      }
+      contactUsPageContactAddress {
+        json
+      }
+    }
+  }
+`
