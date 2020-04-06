da44a967dc7cc088adde5b1b34d837ce0467cf44
devenkhatri <deven.khatri@gmail.com>
2020-04-06 18:52:16 +0530

    Disabled gatsby-plugin-offline so that changes are visible

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
+    //`gatsby-plugin-offline`,
   ],
 }
